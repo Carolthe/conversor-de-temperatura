c4edefea70fc1976d9d96c2bc9fdc8cff7b77233
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-03 23:08:58 +0100

    conversor-de-temperatura

--- src/components/Conversor.jsx
+++ src/components/Conversor.jsx
@@ -4,13 +4,28 @@ export default function Conversor (){
     const [input1, setInput1] = useState ("")
     const [button1, setButton1] = useState("")
 
+    const [input2, setInput2] = useState ("")
+    const [button2, setButton2] = useState ("")
+
     function converterCelcius (ev){
         setInput1(ev.target.value)
+        
     }
     function buttonConversor1 (){
-        setButton1(input1)
+        setButton1(input1 *9/5 +32).toFixed(2) 
         setInput1("")
+    
     }
+    
+    function converterFahrenheit(ev){
+        setInput2(ev.target.value)
+       
+    }
+    function buttonConversor2 (){
+        setButton2(((input2 -32) * 5/9).toFixed(2))
+        setInput2("")
+    }
+
 
     return(
         <>
@@ -26,15 +41,19 @@ export default function Conversor (){
             <button
             onClick={buttonConversor1}
             >Converter</button>
-            <p>Temperatura:{button1}</p>
+            <p>Temperatura em Fahrenheit: {button1}</p>
         </div>
          <div>
          <label>
              Converter de Fahrenheit para Celsius: 
-             <input placeholder="" />
+             <input placeholder=""
+             onChange={converterFahrenheit} 
+             value={input2}
+             type="number"/>
          </label>
-         <button>Converter</button>
-         <p>Temperatura:</p>
+         <button
+         onClick={buttonConversor2}>Converter</button>
+         <p>Temperatura em Celsius: {button2}</p>
      </div>
      </>
     )
